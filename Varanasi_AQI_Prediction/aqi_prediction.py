fca4b823ea55065f8be038cd027dd56f8328b40c
shubhamgitvns <shubhammaurya10092002@gmail.com>
2026-06-19 15:58:24 +0530

    predict the value using linear regression fit the value and predict also

--- Varanasi_AQI_Prediction/aqi_prediction.py
+++ Varanasi_AQI_Prediction/aqi_prediction.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from sklearn.linear_model import LinearRegression
 
 data = pd.read_csv("Varanasi_AQI.csv")
 print(data.tail())
@@ -18,3 +19,19 @@ yearly_aqi = data.groupby('Year')['Index Value'].mean()
 yearly_aqi = yearly_aqi.reset_index()
 
 print(yearly_aqi['Index Value'])
+
+# Start fiting the model values
+x = yearly_aqi[['Year']]
+y = yearly_aqi['Index Value']
+
+model = LinearRegression()
+model.fit(x,y)
+
+print(model)
+print(model.coef_) # slope(m)
+print(model.intercept_) #intercept(b)
+
+new_data = pd.DataFrame({'Year': [2027]})
+
+print(yearly_aqi)
+
